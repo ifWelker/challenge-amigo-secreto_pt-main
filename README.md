88f62cffcf9e5fbcfc4b675d8257bcd10eb9f7e7
ifWelker <fwelker29@outlook.com>
2025-02-03 23:57:36 -0300

    feat: desafio completo

---
+++ README.md
@@ -0,0 +1,32 @@
+# Desafio Amigo Secreto
+
+Este é um projeto desenvolvido durante o curso Oracle ONE da Alura. O objetivo do projeto é criar uma aplicação para sortear amigos secretos.
+
+## Funcionalidades
+
+- Cadastro de participantes
+- Sorteio de amigos secretos
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Como Executar o Projeto
+
+1. Clone o repositório:
+
+2. Navegue até o diretório do projeto:
+   ```bash
+   cd challenge-amigo-secreto_pt-main
+   ```
+3. Abra o arquivo `index.html` no seu navegador.
+
+## Contribuição
+
+Contribuições fechadas, pois o projeto é um desafio do curso Oracle One
+
+## Contato
+
+Para mais informações, entre em contato pelo e-mail: fwelker29@outlook.com
--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
+//  Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Criando a Lista Vazia
+let amigos = [];
+
+//Criando a Function
+function addAmigo() {
+  let amigo = document.getElementById("amigo").value;
+  if (!amigo) {
+    alert("Nome inválido! Favor insirir um nome Válido");
+    return;
+  }
+  amigos.push(amigo);
+
+  const limpaInput = document.getElementById("amigo");
+  limpaInput.value = "";
+
+  listaAmigos();
+
+  console.log("amigo adicionado: ", amigo);
+  console.log("Lista de amigos: ", amigos);
+}
+
+// Função para exibir amigos
+
+function listaAmigos() {
+  let elementoLista = document.getElementById("listaAmigos");
+
+  //Limpa a lista de amigos
+  elementoLista.innerHTML = "";
+
+  // Adiciona cada amigo na lista de amigos usando for
+
+  for (let posicao = 0; posicao < amigos.length; posicao++) {
+    elementoLista.innerHTML += `<li> ${amigos[posicao]} </li>`;
+  }
+}
+
+// Função para sortear amigo
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Adicione amigos para sortear um amigo secreto");
+    return;
+  }
+
+  let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+
+  document.getElementById("resultado").innerHTML = amigoSorteado;
+}
